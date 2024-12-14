787ba87130e8b1b225a8f353e17e9c9f4fb0ff23
Adel Gumama <123368326+Ambatukam2001@users.noreply.github.com>
2024-12-14 22:19:54 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,16 +11,14 @@ if ("serviceWorker" in navigator) {
     navigator.serviceWorker
       .register("/service-worker.js")
       .then((registration) => {
-        console.log(
-          "Service Worker registered with scope:",
-          registration.scope
-        );
+        console.log("Service Worker registered: ", registration);
       })
-      .catch((error) => {
-        console.log("Service Worker registration failed:", error);
+      .catch((registrationError) => {
+        console.log("Service Worker registration failed: ", registrationError);
       });
   });
 }
+
 
 root.render(
   <StrictMode>
